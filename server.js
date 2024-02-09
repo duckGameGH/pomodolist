ff5e3abaf405b99c1639b67af73e7054a9d66df2
hugoDuckGame <pingouinafk255@gmail.com>
2024-02-09 23:09:57 +0100

    Ajouté la lecture d'une page html au server.js Ajouté le <link> vers la feuille de style principale de la page

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('bbb!');
-}).listen(8080);
+const PORT=8080; 
+
+fs.readFile('index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
